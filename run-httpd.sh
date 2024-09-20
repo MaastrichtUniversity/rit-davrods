90f068ffd6a01d7145303e8ce39b7a61f5f43094
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2024-09-20 10:26:17 +0200

    [WIP] First draft of davrods Ubuntu image [DHDO-1438]

--- run-httpd.sh
+++ run-httpd.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-# Make sure we're not confused by old, incompletely-shutdown httpd
-# context after restarting the container.  httpd won't start correctly
-# if it thinks it is already running.
-rm -rf /run/httpd/* /tmp/httpd*
-
 if [ -f /config/davrods-vhost.conf ]; then
-    if [ -f /etc/httpd/conf.d/davrods-vhost.conf ]; then
-        cp /etc/httpd/conf.d/davrods-vhost.conf /etc/httpd/conf.d/davrods-vhost.conf.org
+    if [ -f /etc/apache2/sites-available/davrods-vhost.conf ]; then
+        cp /etc/apache2/sites-available/davrods-vhost.conf /etc/apache2/sites-available/davrods-vhost.conf.org
     fi
-    cp /config/davrods-vhost.conf /etc/httpd/conf.d/davrods-vhost.conf
-    chmod 0644 /etc/httpd/conf.d/davrods-vhost.conf
+    cp /config/davrods-vhost.conf /etc/apache2/sites-available/davrods-vhost.conf
+    chmod 0644 /etc/apache2/sites-available/davrods-vhost.conf
 fi
 
 if [ -f /config/irods_environment.json ]; then
-    cp /config/irods_environment.json /etc/httpd/irods/irods_environment.json
-    chmod 0644 /etc/httpd/irods/irods_environment.json
+    cp /config/irods_environment.json /etc/apache2/irods/irods_environment.json
+    chmod 0644 /etc/apache2/irods/irods_environment.json
 fi
 
 # Start filebeat
@@ -25,4 +20,4 @@ fi
 exec /usr/sbin/apachectl -DFOREGROUND
 
 # this script must end with a persistent foreground process
-tail -F apache.access.log /var/log/httpd/apache.error.log
+tail -F /var/log/apache2/apache.access.log /var/log/apache2/apache.error.log
